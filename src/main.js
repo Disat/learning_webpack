60f15981d9fb909baf469171bfd6bf803cd72973
vhxn <wszxplayers@gmail.com>
2023-07-06 07:52:17 +0800

    feature: 安装配置file-laoder

--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
 import creatHeading from "./heading.js";
 import "./main.css";
+import icon from "./avator.jpg";
 
 const heading = creatHeading();
 
 document.body.append(heading);
+
+const img = new Image();
+img.src = icon;
+document.body.append(img);

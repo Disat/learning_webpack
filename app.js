c98705971b576956ea649cef62a929a50b2c1d15
vhxn <wszxplayers@gmail.com>
2023-07-05 18:41:03 +0800

    测试完成：ES module 用法和注意事项

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-alert("done");
+import { name } from "./module.js";
+// import { hel } from "./module.js";
+// import vhxnage from "./module.js";
+import oVhxn from "./module.js";
+console.log(oVhxn);
+// hel();
+// console.log(vhxnage);
+console.log(name);
+setTimeout(() => {
+  console.log(name);
+}, 2000);
+
+// 导入和导出的是常量
+// name = "Jason";
